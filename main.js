985f37c3202f7219f6d3a9ea30b30e02e4339d40
Bruno C <mail@brunocartier.fr>
2016-08-25 23:12:08 +0200

    Base layout, base map

---
+++ main.js
@@ -0,0 +1,22 @@
+/*jslint browser: true */
+/*global L */
+
+(function () {
+    'use strict';
+
+    var map;
+
+    map = L.map('map').setView([46.160329, -1.151139], 13);
+
+    // Awesome: https://leaflet-extras.github.io/leaflet-providers/preview/
+    L.tileLayer('http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        attribution: 'Imagery from ' +
+            '<a href="http://giscience.uni-hd.de/">' +
+            'GIScience Research Group @ University of Heidelberg' +
+            '</a> &mdash; Map data &copy; ' +
+            '<a href="http://www.openstreetmap.org/copyright">' +
+            'OpenStreetMap' +
+            '</a>'
+    }).addTo(map);
+}());
